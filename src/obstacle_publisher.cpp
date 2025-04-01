a66d06f9c91005967d08411e0ac111d30e7d3bf8
Haowen Yao <haowen.yao@tum.de>
2025-04-01 16:46:01 +0200

    update cmake dependency and library include

--- src/obstacle_publisher.cpp
+++ src/obstacle_publisher.cpp
@@ -1,15 +1,18 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <moveit_msgs/CollisionObject.h>
 #include <moveit_msgs/PlanningScene.h> 
-#include <yaml-cpp/yaml.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Point.h>
 #include <shape_msgs/SolidPrimitive.h>
-#include <fstream>
-#include <eigen3/Eigen/Geometry>
-#include "eigen3/Eigen/Dense"
-#include <ros/package.h>
+
+#include <string>
 #include <vector>
 #include <cmath>
+
+#include <yaml-cpp/yaml.h>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
 
 // moveit_msgs/CollisionObject Message:
 // http://docs.ros.org/en/noetic/api/moveit_msgs/html/msg/CollisionObject.html
@@ -112,7 +115,6 @@ std::vector<Obstacle> readObstacles(const YAML::Node& node)
 
 void publishObstacles(ros::Publisher& pub, const std::vector<Obstacle>& obstacles) 
 {
-    
     moveit_msgs::PlanningScene planning_scene_msg;
     planning_scene_msg.is_diff = true; 
 
